a5cf1750bb326b6269295e73925fb0e69731ae1e
Luiz Poderoso <luizpoderoso72@outlook.com>
2025-08-29 12:22:04 -0300

    feat: charts

--- src/app/musics/_components/button-list/CountByArtistsDialog.jsx
+++ src/app/musics/_components/button-list/CountByArtistsDialog.jsx
@@ -2,6 +2,11 @@
 
 import { Button } from "@/components/ui/button";
 import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import {
   Dialog,
   DialogContent,
   DialogDescription,
@@ -12,11 +17,24 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { countMusicsByArtists } from "@/lib/functional/lib";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Bar, BarChart, XAxis, YAxis } from "recharts";
 
 export default function CountByArtistsDialog({ initialSongs }) {
   const [open, onOpenChange] = useState(false);
-  const groupedByArtist = countMusicsByArtists(initialSongs);
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    const groupedByArtist = Object.entries(countMusicsByArtists(initialSongs));
+    setChartData(groupedByArtist.map(([artist, count]) => ({ artist, count })));
+  }, [initialSongs]);
+
+  const chartConfig = {
+    count: {
+      label: "QTD. Músicas",
+      color: "#eb1243",
+    },
+  };
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
@@ -32,15 +50,24 @@ export default function CountByArtistsDialog({ initialSongs }) {
               Contagem de quantas músicas cada artista tem
             </DialogDescription>
           </DialogHeader>
-          <ul>
-            {Object.entries(groupedByArtist)
-              .sort(([_1, countA], [_2, countB]) => countB - countA)
-              .map(([artist, count], i) => (
-                <li key={artist}>
-                  {i + 1}. {artist}: {count}
-                </li>
-              ))}
-          </ul>
+          {chartData && (
+            <ChartContainer
+              config={chartConfig}
+              className="h-full max-h-[200px]"
+            >
+              <BarChart accessibilityLayer data={chartData}>
+                <ChartTooltip content={<ChartTooltipContent />} />
+                <XAxis
+                  dataKey="artist"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                />
+                <YAxis dataKey="count" tickCount={3} tickMargin={10} />
+                <Bar dataKey="count" fill="var(--color-count)" radius={8} />
+              </BarChart>
+            </ChartContainer>
+          )}
         </DialogContent>
       </DialogPortal>
     </Dialog>
